63a95a15e8493d727af6246291fd024b277f5a8e
Justin Guiel <justinguiel@hal9000.local>
2023-07-28 20:55:09 -0700

    Initialize git repo with flask app, data, and readme

---
+++ README.md
@@ -0,0 +1,69 @@
+# Eikon Therapeutics Technical Assessment
+*Justin Guiel, 2023*
+
+This repo contains a Flask app that is built solely to derive certain features from three CSV files contained withing `data/` directory and upload it to a PostgreSQL database. It is deployed via Docker, including handling the install of all required packages. The data output by the ETL/API are queried via a separate Python script to verify these data are correctly extracted, transformed, and loaded.
+
+## Step-by-step instantiation w/Docker
+1. Clone this repo locally with `git clone <url>`
+2. Enter the repo with `cd eikon_takehome`
+3. Run using `docker run`
+
+### Output
+This script will output a table to your command line, showing the derived data
+
+### Derived data
+From these CSVs, the app derives a new table. For each `user_id`:
+1. `experiments_per_user`: Total experiments ran per user
+2. `avg_experiments_per_user`: The average experiment account for users. Because an average for users was asked, it will be the same number for every `user_id` record
+3. `most_consumed_compound`: The most used compound across all experiments for every user. See *Out of Scope* section for detail on future feature implementation
+
+### Data origin
+This app works explicitly with the three files below.
+```
+eikon_takehome/data/* 
+# This subdirectory contains only the three files below
+users.csv
+user_experiments.csv
+compounds.csv
+```
+If desired, the user can upload their own CSVs. The standard of file name, column name, and value of the CSV should be identical to:
+```
+# users.csv
+"user_id","name","email","signup_date"
+1,STR,"alice@example.com","2023-01-01"
+...
+
+# user_experiments.csv
+"experiment_id","user_id","experiment_compound_ids","experiment_run_time"
+1,"1","1;2","10"
+...
+
+# compounds.csv
+"compound_id","compound_name","compound_structure"
+1,"Compound A","C20H25N3O"
+...
+```
+
+## Out of Scope 
+Fixes and features that I would implement in later refined epics
+- Security
+    - If this were a real app, I would move the database creds into AWS secrets manager or at the very least a dotenv
+- All most-used compounds extracts
+    - Currently, the most used compound by each scientist is shown as the earliest indexed compound
+    - Future feature would have a list of all compounds most used ie. bimodal, trimodal, multimodal most-used compounds
+- Upload data files
+    - Currently, this app ingests whatever files are found in the `data/` directory, and assumes they are named `users.csv`, `user_experiments.csv`, and `compounds.csv`
+    - Future feature would:
+        - Accept POST requests of entire csv files
+        - AWS Lambda listener for files uploaded to specified AWS S3 bucket, ingest these
+        - Handle multiple file extensions ie. Pandas xlsx to csv, etc
+        - Upstream systems uploading these data directly to this app
+- Refine data uploaded to PostgreSQL
+    - Currently, only the derived data is uploaded to the database
+    - Future feature would upload users, user_experiments, and compounds data, depending on scalability
+        - Are these CSVs coming from a novel source which should be uploaded? or are these coming from another database
+        - Redundancy
+        - This would allow querying the origin data to be done by this app—other APIs
+- SQLAlchemy
+    - Currently, pgsql queries are hardcoded and static
+    - Future feature would allow dynamically generated queries for CRUD functions
---
+++ app/app.py
@@ -0,0 +1,134 @@
+""" Small ETL flask app to derive data from user experiments. Justin Guiel 2023 """
+
+import os
+from flask import Flask, request, jsonify
+import pandas as pd
+import psycopg2
+from contextlib import contextmanager
+
+
+# Creds
+# Obviously, these normally would be in a .env file or AWS secrets manager for security
+DB_HOST = 'localhost'
+DB_NAME = 'eikondb'
+DB_USER = 'justin_eikonemployee'
+DB_PASSWORD = 'supersecurepass123'
+app = Flask(__name__)
+
+
+# PostgreSQL Connection, yield as context manager
+@contextmanager
+def pg_conn(host, database, user, password):
+    try:
+        conn = psycopg2.connect(
+            host=host, 
+            database=database, 
+            user=user, 
+            password=password
+        )
+        cursor = conn.cursor()
+        yield cursor
+    finally:
+        conn.commit()
+        cursor.close()
+        conn.close()
+
+
+@app.route('/experiments', methods=['POST'])
+def run_experiment_etl():
+
+    try:
+        # Import and transform
+        path_to_data = request.json['path_to_data']
+        transformed_data = experiment_etl(path_to_data)
+        # Load data to postgres
+        load_data_pg(transformed_data)
+        # Return 200
+        return jsonify({'Success': True,}), 200
+    
+    # Error handling
+    except FileNotFoundError as err:
+        return jsonify({'Bad path to data': str(err)}), 404
+    except Exception as err:
+        print(err)
+        return jsonify({'uh-oh': str(err)}), 500
+    
+
+def experiment_etl(path_to_data: str) -> pd.DataFrame:
+    
+    # Load to pandas dataframes, under assumption of static file name defined in strings here
+    users_df = pd.read_csv(os.path.join(path_to_data, "users.csv"))
+    experiments_df = pd.read_csv(os.path.join(path_to_data, "user_experiments.csv"))
+    compounds_df = pd.read_csv(os.path.join(path_to_data, "compounds.csv"))
+
+    # Instantiate new dict object for derived results
+    derived_results_py = dict()
+
+    # Produce list of ALL user_ids in system
+    user_ids = users_df['user_id'].tolist()
+    derived_results_py['user_id'] = user_ids
+
+    # Find total experiment count for every user_id that has commited experiment
+    total_exp_per_users = experiments_df['user_id'].value_counts()
+    # Add to dict, defaulting to 0 if no experiments found
+    derived_results_py['total_exp_per_user'] = [total_exp_per_users[x] if x in total_exp_per_users.keys() else 0 for x in user_ids]
+
+    # Change experiment_compound_ids in-place to list of ints instead of semicolon-delimited str
+    experiments_df['experiment_compound_ids'] = experiments_df['experiment_compound_ids'].str.split(';').apply(
+        lambda x: list(map(int, x))
+    )
+
+    # Concatenate compounds consumed for all experiments, per user, into singular list
+    all_compounds_consumed_per_user = experiments_df.groupby('user_id')['experiment_compound_ids'].sum()
+    # Create `collections.Counter`-ish object for each user_id to count how many of each compound was consumed
+    compound_id_counts_by_user = all_compounds_consumed_per_user.apply(lambda x: pd.Series(x).value_counts())
+
+    # Select user's most used compound_id
+    most_consumed_compound_id = compound_id_counts_by_user.idxmax(axis=1)
+    # Must be declared as series of dtype=object, to handle both int and NaN
+    derived_results_py["most_consumed_compound_id"] = pd.Series(
+        [int(most_consumed_compound_id[x]) if x in most_consumed_compound_id.keys() else None for x in user_ids], 
+        dtype=object
+    )
+    # Select user's most used compound_name
+    most_consumed_compound = most_consumed_compound_id.map(compounds_df.set_index('compound_id')['compound_name'])
+    derived_results_py["most_consumed_compound_name"] = [most_consumed_compound[x] if x in most_consumed_compound.keys() else None for x in user_ids]
+    
+    # DataFramed for ease of ingestion to pgsql
+    derived_results = pd.DataFrame(derived_results_py)
+    print(derived_results)
+    return derived_results
+
+
+def load_data_pg(derived_results: pd.DataFrame) -> None:
+
+    
+    # Connection as context mgr
+    with pg_conn(DB_HOST, DB_NAME, DB_USER, DB_PASSWORD) as cursor:
+        # Create table
+        create_table_query = """
+        CREATE TABLE IF NOT EXISTS derived_exp_data (
+            user_id INT,
+            total_exp_per_user INT,
+            most_consumed_compound INT,
+            most_consumed_compound_name TEXT
+        )
+        """
+        cursor.execute(create_table_query)
+
+        # Iterate through df and insert to new pg table
+        for _, user_data in derived_results.iterrows():
+            insert_query = """
+            INSERT INTO derived_exp_data (
+                user_id,
+                total_exp_per_user,
+                most_consumed_compound,
+                most_consumed_compound_name
+            ) VALUES (%s, %s, %s, %s)
+            """
+            values = tuple(user_data)
+            cursor.execute(insert_query, values)
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
